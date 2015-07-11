7953469d22fc78dec83b00858ea486560c1e3481
mrameezraja <mrameezraja@gmail.com>
2015-07-11 15:18:57 +0500

    updated

--- www/gps.js
+++ www/gps.js
@@ -4,26 +4,17 @@ var GPS = function() {
 };
 
 GPS.prototype.getStatus = function (successCallback, errorCallback) {
-    var onsuccess = function(contacts){
-      successCallback(JSON.parse(contacts));
+    var onsuccess = function(gpsStatus){
+      successCallback(JSON.parse(gpsStatus));
     }
     cordova.exec(onsuccess, errorCallback, "GPS", "getStatus");
 }
 
 GPS.prototype.getCurrentLocation = function (successCallback, errorCallback) {
-    var onsuccess = function(contacts){
-      successCallback(JSON.parse(contacts));
+    var onsuccess = function(location){
+      successCallback(JSON.parse(location));
     }
     cordova.exec(onsuccess, errorCallback, "GPS", "getCurrentLocation");
 }
 
-if(!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.gps) {
-    window.plugins.gps = new GPS();
-}
-
-if(window.module){
-  module.exports = GPS;
-}
+module.exports = GPS;
