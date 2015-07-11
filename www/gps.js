668079613bdd24a30045bd3e6814f3b016ade769
mrameezraja <mrameezraja@gmail.com>
2015-07-11 15:39:19 +0500

    updated

--- www/gps.js
+++ www/gps.js
@@ -1,20 +1,24 @@
 
+var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
 var gps = function() {
 
 };
 
-gps.getStatus = function (successCallback, errorCallback) {
+gps.prototype.getStatus = function (successCallback, errorCallback) {
     var onsuccess = function(gpsStatus){
       successCallback(JSON.parse(gpsStatus));
     }
-    cordova.exec(onsuccess, errorCallback, "GPS", "getStatus");
+    exec(onsuccess, errorCallback, "GPS", "getStatus");
 }
 
-gps.getCurrentLocation = function (successCallback, errorCallback) {
+gps.prototype.getCurrentLocation = function (successCallback, errorCallback) {
     var onsuccess = function(location){
       successCallback(JSON.parse(location));
     }
-    cordova.exec(onsuccess, errorCallback, "GPS", "getCurrentLocation");
+    exec(onsuccess, errorCallback, "GPS", "getCurrentLocation");
 }
 
 module.exports = gps;
